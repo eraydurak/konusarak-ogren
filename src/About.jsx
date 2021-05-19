c61e0fb9d614bcce9e9f38aa5a66494d1fbcc090
Eray Durak <eraydurak1@outlook.com>
2021-05-20 02:02:20 +0300

    Added About component

--- src/About.jsx
+++ src/About.jsx
@@ -1,23 +1,43 @@
 const About = () => {
-  return(
+  return (
     <>
-    <h1 className="big-header">HAKKIMDA</h1>
-    <div className="about-container">
-      <div className="about-top">
-        <h4>X</h4>
-        <ul>
-          <li>
-            _
+      <h1 className="big-header">HAKKIMDA</h1>
+      <div className="about-container">
+        <div className="about-top">
+          <h4>X</h4>
+          <ul>
+            <li>
+              _
           </li>
-          <li>
-            ?
+            <li>
+              ?
           </li>
-          <li>
-            ◼
+            <li>
+              ◼
           </li>
-        </ul>
+          </ul>
+        </div>
+        <div className="paraDiv">
+          <p>I am a self-taught Front-End Developer.While studying Metallurgical and Materials Engineering, I chose a different career path that led me to the JavaScript.</p><br />
+          <span>Technologies I use :</span><br /><br />
+          <p>HTML5, CSS3, SASS, JavaScript(ES6), ReactJS, Redux, Webpack, NPM, Git, Bootstrap, Restful API, Fetch API, Redux-Thunk, CSS-in-JS.</p><p>Currently working with Typescript.</p><br />
+          <span>My areas of expertise :</span><br /><br />
+          <ul>
+            <li>
+              Functional and OO Programming in JavaScript
+            </li>
+            <li>
+              Teamwork and Cross-functional Communication
+            </li>
+            <li>
+              Entrepreneurship
+            </li>
+            <li>
+              Problem Solving in Complex Projects
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
     </>
   );
 }
