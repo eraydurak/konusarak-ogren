d7d2b25003d16b091dd448ded6b94d23dcf37804
Eray Durak <eraydurak1@outlook.com>
2021-05-20 02:03:07 +0300

    Added components to mainpage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Navbar from './Navbar';
 import About from './About';
+import Calismalar from './Calismalar';
+import Iletisim from './Iletisim';
 import './App.scss';
+import YorumEkle from './YorumEkle';
 
 function App() {
   return (
     <>
       <Navbar />
       <About />
+      <Calismalar />
+      <Iletisim />
+      <YorumEkle />
     </>
   );
 }
