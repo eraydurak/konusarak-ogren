cb2a268848c8a1139f4efa1a4ce0a8fa63ad0d22
Eray Durak <eraydurak1@outlook.com>
2021-05-19 22:35:42 +0300

    Completed Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,35 @@
+
+
 const Navbar = () => {
   return(
     <nav>
-      Hello
+      <div className="logo">
+        <h1><span>E</span><span>R</span><span>A</span><span>Y</span></h1>
+      </div>
+      <div className="navbar">
+        <ul>
+          <li>
+            <a href="#">
+              hakkimda
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              yaptigimIsler
+            </a>
+          </li>
+          <li>
+            <a href="#">
+            iletisim
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              yorumlar
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
