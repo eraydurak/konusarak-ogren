9c22c5f36ee8e040f339db5582bb6704f9926a40
Eray Durak <eraydurak1@outlook.com>
2021-05-20 02:02:35 +0300

    Added Iletisim component

---
+++ src/Iletisim.jsx
@@ -0,0 +1,43 @@
+const Iletisim = () => {
+  return (
+    <>
+      <h1 className="big-header">ILETISIM</h1>
+      <div className="contact" id="contact">
+        <div className="about-top">
+          <h4>X</h4>
+          <ul>
+            <li>
+              _
+            </li>
+            <li>
+              ?
+            </li>
+            <li>
+              ◼
+            </li>
+          </ul>
+        </div>
+        <div className="logo-boxes">
+          <div className="logo-box">
+            <i class="fab fa-twitter"></i>
+            <a href="https://twitter.com/eraydurakk" target="_blank" rel="noreferrer">Twitter</a>
+          </div>
+          <div className="logo-box">
+            <i class="fab fa-linkedin"></i>
+            <a href="https://www.linkedin.com/in/eraydurak/" target="_blank" rel="noreferrer">LinkedIn</a>
+          </div>
+          <div className="logo-box">
+            <i class="far fa-envelope"></i>
+            <a href="mailto: eraydurak1@outlook.com" target="_blank" rel="noreferrer">E-Mail</a>
+          </div>
+          <div className="logo-box">
+            <i class="fas fa-mobile-alt"></i>
+            <a href="tel: +905312541106">Telefon</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Iletisim;
